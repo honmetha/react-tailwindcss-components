7c883d12cfee0b8c68c40a4cfa184e41a6e4bf04
Hon <dennis.mejen@gmail.com>
2021-04-05 23:57:30 +0700

    [Edited] Text

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const NavigationBar = () => (
-  <header className="bg-gray-200 w-full h-16">asdfasdf</header>
+  <header className="bg-gray-200 w-full h-16">Navigation Bar</header>
 );
 
 export default NavigationBar;
