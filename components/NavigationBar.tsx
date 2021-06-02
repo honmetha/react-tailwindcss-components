9f2e4bcf69553b064ba55622c9e2bfa4a69eea40
Hon <dennis.mejen@gmail.com>
2021-06-02 10:53:10 +0700

    [Added] Animated dropdown menu

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -4,12 +4,23 @@ const NavigationBar = () => (
   <header className="bg-gray-200 w-full h-16">
     <ul className="flex items-center justify-between h-full">
       {footerLinks.map((item) => (
-        <li>
+        <li className="mega-menu relative">
           <span>{item.header}</span>
           <span className="animated-arrow">
             <span></span>
             <span></span>
           </span>
+          <ul className="absolute top-11 -left-4 dropdown_menu dropdown_menu-2">
+            {item.subMenu.map((menu, index) => (
+              <li
+                className={`hidden opacity-0 bg-gray-200 dropdown_item-${
+                  index + 1
+                }`}
+              >
+                {menu.label}
+              </li>
+            ))}
+          </ul>
         </li>
       ))}
     </ul>
