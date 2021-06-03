cb98c808e41e451ec6c9cdb66c76e5b59265a84f
Hon <dennis.mejen@gmail.com>
2021-06-03 15:11:19 +0700

    [Updated] SCSS -> Tailwinds CSS

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,11 @@ const Footer = () => {
         Click here to expand
       </h2>
       <ul
-        className={isExpanded ? "footer-list footer-expanded" : "footer-list"}
+        className={
+          isExpanded
+            ? "max-h-0 transition-all duration-300 ease-in overflow-hidden max-h-96"
+            : "max-h-0 transition-all duration-300 ease-in-out overflow-hidden"
+        }
       >
         <li>List 1</li>
         <li>List 2</li>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import "../styles/tailwind.css";
 import "../styles/NavigationBar.scss";
-import "../styles/Footer.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
